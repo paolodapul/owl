87086b41b45ee308e0a54394b019e6753febf99f
Paolo Dapul <paolodapultech@gmail.com>
2022-07-20 20:56:27 +0800

    Configure import linting

--- packages/eslint-config-owl/index.js
+++ packages/eslint-config-owl/index.js
@@ -1,6 +1,24 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    project: 'packages/*/tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: ['packages/*/tsconfig.json', 'apps/*/tsconfig.json'],
+      },
+    },
+  },
 }
