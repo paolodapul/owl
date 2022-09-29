f65b4c9c8a2f80df7eb640fafd5b83e3aaa1b89d
Paolo Dapul <paolodapultech@gmail.com>
2022-09-29 22:21:19 +0800

    Fix import resolution for React + Vite

--- apps/web/src/components/App/App.tsx
+++ apps/web/src/components/App/App.tsx
@@ -1,15 +1,7 @@
-// Todo: Fix import resolution
-// import { Home } from '@components/Home'
+import { Home } from '@components/Home'
 
 function App() {
-  return (
-    <>
-      <div>
-        Owl - a full-stack TypeScript web app starter template, made with ❤️ by
-        @paolodapul
-      </div>
-    </>
-  )
+  return <Home />
 }
 
 export { App }
--- apps/web/src/components/Home/Home.constants.ts
+++ apps/web/src/components/Home/Home.constants.ts
@@ -1,1 +1,1 @@
-export const HEADING = 'Home page'
+export const HEADING = 'Owl - a full-stack TypeScript web app starter template'
