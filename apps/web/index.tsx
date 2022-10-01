724fa385b1b1986ec60828ae9b27a5ecc53c2444
Paolo Dapul <paolodapultech@gmail.com>
2022-10-01 16:47:36 +0800

    Setup React Router 6

--- apps/web/index.tsx
+++ apps/web/index.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './src/components/App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const container = document.getElementById('app');
 const root = createRoot(container as HTMLElement);
-root.render(<App />);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
