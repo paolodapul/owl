1d0ea90c50b286ac0348aae94006957fa1f2370a
Paolo Dapul <paolodapultech@gmail.com>
2022-10-21 14:17:07 +0800

    Configure TypeGraphQL

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,43 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
+import 'reflect-metadata';
+import { PrismaClient } from '@prisma/client';
+import {
+  ObjectType,
+  Resolver,
+  Query,
+  Field,
+  buildSchemaSync,
+} from 'type-graphql';
+import { graphqlHTTP } from 'express-graphql';
+
+const prisma = new PrismaClient();
+
+@ObjectType()
+export class User {
+  @Field()
+  email?: string;
+
+  @Field((type) => String, { nullable: true })
+  name?: string | null;
+}
+
+@Resolver(User)
+export class UserResolver {
+  @Query((returns) => [User], { nullable: true })
+  async allUsers() {
+    return prisma.user.findMany();
+  }
+}
+
+const schema = buildSchemaSync({
+  resolvers: [UserResolver],
+  emitSchemaFile: true,
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }) as RequestHandler);
 
 app.get('/', (req, res) => {
   res.send({ data: 'hello, world!' });
