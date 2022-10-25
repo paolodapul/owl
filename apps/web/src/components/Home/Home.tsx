cc4734e35ea963947ad8af9bc352a6735394bb6c
Paolo Dapul <paolodapultech@gmail.com>
2022-10-25 20:59:11 +0800

    Setup static asset importing

--- apps/web/src/components/Home/Home.tsx
+++ apps/web/src/components/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { HEADING } from './Home.constants';
 import { getCurrentDateToLocaleString } from './Home.helpers';
+import { ReactComponent as Icon } from '@assets/o.svg';
 
 function Home() {
   return (
     <div className="text-sm">
+      <Icon style={{ height: '50px', width: '50px' }} />
       <div>{HEADING}</div>
       <p>Today is: {getCurrentDateToLocaleString()}</p>
     </div>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,9 +3,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [svgr(), react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
